efd74d7484428fb232ae1b221261d5cb4b618aaa
Xmaxkambek <maxkambekxolbekov515@gmail.com>
2023-01-19 15:43:40 +0500

    the end

--- lib/widget/chart.dart
+++ lib/widget/chart.dart
@@ -49,18 +49,24 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Padding(
         padding: EdgeInsets.all(10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: groupedTransactionValues.map((data) {
-            return ChartBar(
-              data['day'] as String,
-              data['amount'] as double,
-               totalSpending == 0.0
-                    ? 0.0
-                    : (data['amount'] as double) / totalSpending,
-              
-            );
-          }).toList(),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactionValues.map((data) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(
+                  data['day'] as String,
+                  data['amount'] as double,
+                   totalSpending == 0.0
+                        ? 0.0
+                        : (data['amount'] as double) / totalSpending,
+                  
+                ),
+              );
+            }).toList(),
+          ),
         ),
       ),
     );
