3cb8e78dbc03e10fd96804fbcf275c1c4fe03051
Xmaxkambek <maxkambekxolbekov515@gmail.com>
2023-01-19 15:15:34 +0500

    the end

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_lesson/widget/chart.dart';
 import './models/transaction.dart';
 import 'widget/new_transaction.dart';
 import 'widget/transaction_list.dart';
+import './widget/chart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Personal Expenses',
       theme: ThemeData(
         primarySwatch: Colors.purple,
@@ -28,26 +31,35 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-     't1',
-      69.99,
-     DateTime.now(),
-       'New Shoes',
-    ),
-    Transaction(
-'t2',
-      16.53,
-      DateTime.now(),
-   'Weekly Groceries',
-    ),
+//     Transaction(
+//      't1',
+//       69.99,
+//      DateTime.now(),
+//        'New Shoes',
+//     ),
+//     Transaction(
+// 't2',
+//       16.53,
+//       DateTime.now(),
+//    'Weekly Groceries',
+//     ),
   ];
+  List<Transaction> get _recensTransaction {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
-     txTitle,
-     txAmount,
-     DateTime.now(),
-   DateTime.now().toString(),
+      txTitle,
+      txAmount,
+      DateTime.now(),
+      DateTime.now().toString(),
     );
 
     setState(() {
@@ -70,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Personal Expenses'),
         actions: <Widget>[
@@ -85,14 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.purple,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recensTransaction),
             TransactionList(_userTransactions),
           ],
         ),
